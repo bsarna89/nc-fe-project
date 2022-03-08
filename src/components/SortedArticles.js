d1cfa25a14368e5de6a288d27175ec6be9db5599
Bartosz <bsarna89@gmail.com>
2022-03-08 17:08:53 +0000

    sorting setup

--- src/components/SortedArticles.js
+++ src/components/SortedArticles.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const SortedArticles = () => {
+
+
+    const [sortParameters, setSortParameters] = useState();
+
+
+
     return (
         <div>
-            <p> sorted articles by params</p>
+            <form>
+                <label>Choose articles by:</label>
+                <select>
+                    <option value="created_at">Date</option>
+                    <option value="article_id">ID</option>
+                    <option value="votes">Votes</option>
+                    <option value="comment_count">Number of Commets</option>
+                </select>
+
+            </form>
         </div>
     );
 };
