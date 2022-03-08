2a29b5abd1bab31ce7b22618e7f43f74fbd1a43b
Bartosz <bsarna89@gmail.com>
2022-03-08 12:37:45 +0000

    view topics

---
+++ src/components/SingleTopic.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchArticlesByTopic } from '../utils/api';
+import Loader from './Loader';
+import { Link } from 'react-router-dom';
+
+const SingleTopic = () => {
+
+    const { params } = useParams();
+
+    const [articles, setArticlesByTopic] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    const urlString = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6fIN36i6PHWoGOdaalRtIXnbTbPdkYe2FtzvgV-cYvZDEZDn8M1qL56S3hcrmSdFaNw&usqp=CAU";
+
+
+    useEffect(() => {
+        fetchArticlesByTopic(params).then(({ articles }) => {
+
+            setArticlesByTopic(articles);
+            setLoading(false);
+        })
+
+    }, [params]);
+
+
+
+    if (isLoading) return (<Loader> </Loader>);
+
+    return (
+        <div>
+            <ul className='list'>
+                {articles.map((article) => {
+                    return (
+                        <li key={article.article_id} className='title' >
+                            <p> {article.title}</p>
+                            <p> Topic :{article.topic}</p>
+                            <Link to="/articles/id" className='link'>
+                                <img src={urlString} className='go-img' alt="Read Article"></img>
+                            </Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default SingleTopic;
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,29 @@ import axios from "axios";
 
 export function fetchArticles() {
 
+
     return axios
-        .get("https://nc-news-b.herokuapp.com/api/articles")
+        .get(`https://nc-news-b.herokuapp.com/api/articles`)
+        .then((response) => {
+
+            return response.data;
+        });
+}
+
+export function fetchTopics() {
+
+    return axios
+        .get("https://nc-news-b.herokuapp.com/api/topics")
+        .then((response) => {
+
+            return response.data;
+        });
+}
+
+export function fetchArticlesByTopic(params) {
+
+    return axios
+        .get(`https://nc-news-b.herokuapp.com/api/articles/?topic=${params}`)
         .then((response) => {
 
             return response.data;
